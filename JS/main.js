fbd3398a6db8a37af60b24eea3b30d8a8ec3d4b5
0andme <yu00mi97@gmail.com>
2021-10-02 02:34:28 +0900

    feat : JS 수정
    + 배지 요소 스크롤 내리면 사라짐
    + 비쥬얼 요소들 순서대로 나타남

--- JS/main.js
+++ JS/main.js
@@ -7,14 +7,48 @@ searchEl.addEventListener("click", function () {
 })
 
 searchInputEL.addEventListener("focus", function () {
-  
+
   searchEl.classList.add("focused");
-  searchInputEL.setAttribute("placeholder","통합검색");
+  searchInputEL.setAttribute("placeholder", "통합검색");
 
 })
 searchInputEL.addEventListener("blur", function () {
-  
+
   searchEl.classList.remove("focused");
-  searchInputEL.setAttribute("placeholder","");
+  searchInputEL.setAttribute("placeholder", "");
+
+});
+
+// ! 배지
+const badgeEl = document.querySelector(" header .badges");
+// window.addEventListener("scroll",function(){
+//   console.log(window.scrollY);
+// })
+window.addEventListener("scroll", _.throttle(function () {
+  // console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // badgeEl.style.display="none";
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: "none"
+    });
+  } else {
+    //배지 보이기
+    // badgeEl.style.display="block";
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: "block"
+    });
+  }
+}, 300));
+
+// ! 요소 순서대로 나타내기
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
 
 })
